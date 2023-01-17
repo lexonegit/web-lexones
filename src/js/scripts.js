d0db79b19df9c384561630720bdda9c0f93f8bfe
Leevi <leeviftw@hotmail.com>
2023-01-17 17:45:51 +0200

    2023 revision

---
+++ src/js/scripts.js
@@ -0,0 +1,28 @@
+const themeToggle = document.querySelector("#theme-input");
+const themeState = localStorage.getItem("themeState") == "true";
+
+const setTheme = () => {
+    document.documentElement.setAttribute("theme", themeToggle.checked ? "light" : "dark");
+}
+
+themeToggle.addEventListener("change", () => {
+    setTheme();
+    localStorage.setItem("themeState", themeToggle.checked);
+});
+
+themeToggle.checked = themeState;
+setTheme();
+
+const copyMail = () => {
+    let m = getString();
+    navigator.clipboard.writeText(m);
+    alert("Copied email address to the clipboard!");
+}
+
+const getString = () => {
+    // In hexadecimal to avoid bots
+    let x = "006c0065006500760069002e00730065007000700061006c0061003100400067006d00610069006c002e0063006f006d";
+    return x.match(/.{1,4}/g).reduce((result, hex) => result + String.fromCharCode(parseInt(hex, 16)), "");
+}
+
+document.querySelector("#email-container").style.display = "flex";

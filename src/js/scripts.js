98504ca0b9a6945854e7fbed1839c3ed636a3cbe
Leevi <leeviftw@hotmail.com>
2023-01-24 11:24:46 +0200

    Fix clipboard copy not always working

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,12 +15,13 @@ setTheme();
 
 const copyMail = () => {
     let m = getString();
-    navigator.clipboard.writeText(m);
-    alert("Copied email address to the clipboard!");
+    navigator.clipboard.writeText(m).then(() => {
+        alert("Copied email address to the clipboard!");
+    });
 }
 
 const getString = () => {
-    // In hexadecimal to avoid bots
+    // In hexadecimal to avoid spam bots from scraping
     let x = "006c0065006500760069002e00730065007000700061006c0061003100400067006d00610069006c002e0063006f006d";
     return x.match(/.{1,4}/g).reduce((result, hex) => result + String.fromCharCode(parseInt(hex, 16)), "");
 }

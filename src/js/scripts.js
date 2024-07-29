4fcfc7ef60c3d9b6d975e9454851bbce2eb7a63e
lexonegit <leeviftw@hotmail.com>
2024-07-29 16:22:02 +0300

    Rework design, update project listings and improve everything overall

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,29 +1,30 @@
+// Theme toggling
 const themeToggle = document.querySelector("#theme-input");
 const themeState = localStorage.getItem("themeState") == "true";
 
 const setTheme = () => {
-    document.documentElement.setAttribute("theme", themeToggle.checked ? "light" : "dark");
-}
+  document.documentElement.setAttribute("data-theme", themeToggle.checked ? "light" : "dark");
+};
 
 themeToggle.addEventListener("change", () => {
-    setTheme();
-    localStorage.setItem("themeState", themeToggle.checked);
+  setTheme();
+  localStorage.setItem("themeState", themeToggle.checked);
 });
 
 themeToggle.checked = themeState;
 setTheme();
 
-const copyMail = () => {
-    let m = getString();
-    navigator.clipboard.writeText(m).then(() => {
-        alert("Copied email address to the clipboard!");
-    });
-}
+// Email stuff (bot prevention)
+// 07/2024: No longer used (scrape away!)
+const copy = () => {
+  let m = getString();
+  navigator.clipboard.writeText(m).then(() => {
+    // alert("Copied email address to the clipboard!");
+  });
+};
 
 const getString = () => {
-    // In hexadecimal to avoid spam bots from scraping
-    let x = "006c0065006500760069002e00730065007000700061006c0061003100400067006d00610069006c002e0063006f006d";
-    return x.match(/.{1,4}/g).reduce((result, hex) => result + String.fromCharCode(parseInt(hex, 16)), "");
-}
-
-document.querySelector("#email-container").style.display = "flex";
+  // Hide email in hex format
+  let x = "006c0065006500760069002e00730065007000700061006c0061003100400067006d00610069006c002e0063006f006d";
+  return x.match(/.{1,4}/g).reduce((result, hex) => result + String.fromCharCode(parseInt(hex, 16)), "");
+};
